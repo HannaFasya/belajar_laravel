1b78934191f53cd4be4b1834c9ed9c9701c18c05
HannaFasya <hanna.firdausiah20@gmail.com>
2022-08-26 09:01:02 +0700

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,75 @@
+
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel #8 : Sistem Template Blade Laravel</title>
+</head>
+<body>
+ 
+	<header>
+ 
+		<h2>Blog MalasNgoding</h2>
+		<nav>
+			<a href="/blog">HOME</a>
+			|
+			<a href="/blog/tentang">TENTANG</a>
+			|
+			<a href="/blog/kontak">KONTAK</a>
+		</nav>
+	</header>
+	<hr/>
+	<br/>
+	<br/>
+ 
+	<!-- bagian judul halaman blog -->
+	<h3> @yield('judul_halaman') </h3>
+ 
+ 
+	<!-- bagian konten blog -->
+	@yield('konten')
+ 
+ 
+	<br/>
+	<br/>
+	<hr/>
+	<footer>
+		<p>&copy; <a href="https://www.malasngoding.com">www.malasngoding.com</a>. 2018 - 2019</p>
+	</footer>
+ 
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('halo', function () {
+	return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+Route::get('blog', function () {
+	return view('blog');
+});
+Route::get('dosen', 'App\Http\Controllers\DosenController@index');
+// Route::get('/pegawai/{nama}', 'PegawaiController@index');
+Route::get('/formulir', 'PegawaiController@formulir');
+Route::post('/formulir/proses', 'PegawaiController@proses');
+
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/cari','PegawaiController@cari');
+Route::get('/input', 'HannaController@input');
+Route::post('/proses', 'HannaController@proses');
+
+	
+Route::get('/pegawai', 'PegawaiController@index');
+Route::get('/pegawai/tambah', 'PegawaiController@tambah');
+Route::post('/pegawai/store', 'PegawaiController@store');
+Route::get('/pegawai/edit/{id}', 'PegawaiController@edit');
+Route::put('/pegawai/update/{id}', 'PegawaiController@update');
+Route::get('/pegawai/hapus/{id}', 'PegawaiController@delete');
